48079efd8061e3d3169fcbe7d70f95f956dc43c8
Grisha55 <grishavinyar@icloud.com>
2025-06-15 22:18:54 +0300

    Add 0062-Unique-Paths solution

---
+++ 0062-Unique-Paths/Solution.swift
@@ -0,0 +1,22 @@
+//
+//  0062. Unique Paths
+//  Problem: https://leetcode.com/problems/unique-paths/
+//  SwiftLeetCode
+//
+
+import Foundation
+
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        var dp = Array(repeating: 1, count: n)
+
+        for _ in 1..<m {
+            for j in 1..<n {
+                dp[j] += dp[j - 1]
+            }
+        }
+
+        return dp[n - 1]
+    }
+
+}
