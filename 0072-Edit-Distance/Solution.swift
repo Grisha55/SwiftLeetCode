968c6351a24c4994d44c0806860806cd73da90b4
Gregory Vinyar <grishavinyar64@gmail.com>
2025-06-16 13:48:02 +0800

    Update Solution.swift

--- 0072-Edit-Distance/Solution.swift
+++ 0072-Edit-Distance/Solution.swift
@@ -12,22 +12,30 @@ class Solution {
         var b = Array(word2)
         let m = a.count
         let n = b.count
-        var dp = Array(repeating: Array(repeating: 0, count: n + 1), count: m + 1)
         
+        // dp[i][j] - is the minimum number of operations to turn the first i characters of word1 into the first j characters of word2.
+        var dp = Array(repeating: Array(repeating: 0, count: n + 1), count: m + 1)
+
+        // If word2 is empty (j == 0), then we need i delete operations from word1 to get an empty string.
         for i in 0...m {
             dp[i][0] = i
         }
 
+        // If word1 is empty (i == 0), it takes j insertions to get word2.
         for j in 0...n {
             dp[0][j] = j
         }
         if m > 0 && n > 0 {
             for i in 1...m {
                 for j in 1...n {
+                    // If the characters are the same, no operation is needed, so we take the value on the diagonal dp[i-1][j-1].
                     if a[i - 1] == b[j - 1] {
                         dp[i][j] = dp[i - 1][j - 1]
                     } else {
-                        dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+                        // If different, we take the minimum of these three:
+                        dp[i][j] = 1 + min(dp[i - 1][j],      // delete
+                                           dp[i][j - 1],      // insert
+                                           dp[i - 1][j - 1])  // replace
                     }
                 }
             }
