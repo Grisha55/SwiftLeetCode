1eef790afad6309198175dc9a8e2adc4e5300b5e
Gregory Vinyar <grishavinyar64@gmail.com>
2025-07-01 15:48:33 +0800

    Update Solution.swift

--- 3330-Find-the-Original-Typed-String-I/Solution.swift
+++ 3330-Find-the-Original-Typed-String-I/Solution.swift
@@ -2,25 +2,14 @@ class Solution {
     func possibleStringCount(_ word: String) -> Int {
         let chars = Array(word)
         let n = chars.count
-        var i = 0
-        var repeatGroups = 0
+        var copies = 1
 
-        while i < n {
-            var j = i
-            // Move 'j' forward while the characters are the same
-            while j < n && chars[j] == chars[i] {
-                j += 1
+        for i in 1..<n {
+            if chars[i - 1] == chars[i] {
+                copies += 1
             }
-            let groupLen = j - i
-            if groupLen > 1 {
-                // Each group of length L gives (L - 1) compression options
-                repeatGroups += groupLen - 1
-            }
-            i = j
         }
 
-        // We can either compress one group (in multiple ways),
-        // or leave everything as-is
-        return repeatGroups + 1
+        return copies
     }
 }
