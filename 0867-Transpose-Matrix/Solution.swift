cf78e217f302e58b0fe9c558d35463960aa45b54
Grisha55 <grishavinyar@icloud.com>
2025-08-11 10:41:50 +0300

    Added Solution.swift

---
+++ 0867-Transpose-Matrix/Solution.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func transpose(_ matrix: [[Int]]) -> [[Int]] {
+        let m = matrix.count
+        let n = matrix[0].count
+        var result = Array(repeating: Array(repeating: 0, count: m), count: n)
+
+        for i in 0..<m {
+            for j in 0..<n {
+                result[j][i] = matrix[i][j]
+            }
+        }
+
+        return result
+    }
+}
