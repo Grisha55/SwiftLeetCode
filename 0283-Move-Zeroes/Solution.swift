791185352b26abdcc86f262198a5392ea52ae7d7
Grisha55 <grishavinyar@icloud.com>
2025-08-19 01:29:18 +0300

    Added Solution.swift

---
+++ 0283-Move-Zeroes/Solution.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func moveZeroes(_ nums: inout [Int]) {
+        var index = 0
+
+        for i in 0..<nums.count {
+            if nums[i] != 0 {
+                nums[index] = nums[i]
+                index += 1
+            } 
+        } 
+
+        for i in index..<nums.count {
+            nums[i] = 0
+        }
+    }
+}
